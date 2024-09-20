63839f09989ac6d63170ade16ce9a8c4191e0c84
thomp2e3 <thomp2e3@mail.uc.edu>
2024-09-19 23:17:41 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+    int numToExamine = 2;
+
+    if (numToExamine % 2 == 0) {
+        System.out.println(numToExamine + " is even!");
+    }
+    else {
+        System.out.println(numToExamine + " is odd!");
+    }
+    }
+}
